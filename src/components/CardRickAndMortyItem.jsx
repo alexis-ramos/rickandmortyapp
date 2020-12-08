ba8c30a325ae00d8cefd5f4ade8b07eb2b4182c1
Alexis Ramos <alexisramoscaballero@gmail.com>
2020-12-08 17:06:16 -0600

    Se agrega el diseno del card

--- src/components/CardRickAndMortyItem.jsx
+++ src/components/CardRickAndMortyItem.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
 
 const CardRickAndMortyItem = () => (
-  <div className='min-h-screen bg-gray-400 mx-4 mb-8'>
-    <div>
-      <img src='' alt='' />
+  <div className='flex bg-red-400  '>
+    <div className='flex-none w-1/2 relative'>
+      <img
+        className='absolute inset-0 w-full h-full object-cover'
+        src='https://picsum.photos/seed/picsum/200/300'
+        alt='img'
+      />
     </div>
-    <div></div>
+    <div className='w-1/2 text-center'>
+      <div>
+        <h1 className='font-semibold text-white text-4xl'>Nombre</h1>
+        <span className=''>
+          <span className='bg-green-600'>s</span>
+          Vivo - Alien
+        </span>
+        <div className='pt-2'>
+          <p className=' text-gray-700'>de</p>
+          <p className='text-white'>ded</p>
+        </div>
+        <div className='pt-2'>
+          <p className=' text-gray-700'>de</p>
+          <p className='text-white'>ded</p>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
